1e036b4515e516e7f8cfb42c9defd5ac8bb7c585
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-01-04 16:36:44 -0300

    feat: paths

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs'
 
 import { Slug } from "./value-objects/slug"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
-import { Entity } from "../../core/entities/entity"
-import { Optional } from "../../core/types/optional"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Entity } from "@/core/entities/entity"
+import { Optional } from "@/core/types/optional"
 
 interface QuestionProps {
   authorId: UniqueEntityID
